0e4e1a5c1bc5a4866ecab069bbf8fed8243d02c4
hardglitch <lightwave6666@gmail.com>
2023-11-23 15:20:43 +0500

    GProgression.get_q + tests

--- src/math.rs
+++ src/math.rs
@@ -2,52 +2,53 @@ use std::error::Error;
 
 
 #[allow(dead_code)]
-pub fn factorial(n: usize) -> u128 {
+pub fn factorial(n: &usize) -> u128 {
     //! n! = 2 * 3 * ... * n
 
-    match n {
+    match *n {
         0 => { 0 }
         1 => { 1 }
         _ => {
-            let mut r: u128 = 1;
-            for i in 2..=n as u128 {
-                r *= i
+            let mut r = Box::<u128>::new(1);
+            for i in 2..=*n as u128 {
+                *r *= i
             }
-            r
+            *r
         }
     }
 }
 
 
 #[allow(dead_code)]
-pub fn binominal_coefficient(m: usize, n: usize) -> Result<f64, Box<dyn Error>> {
+pub fn binominal_coefficient(m: &usize, n: &usize) -> Result<f64, Box<dyn Error>> {
     //! C_n^m = n! / m! * (n - m)!
 
     if n < m { return Err("Argument 'n' must be greater then 'm'.".into()); }
 
-    let mut opt_n: u128 = 1;
-    for i in m+1..=n {
-        opt_n *= i as u128;
+    let mut opt_n = Box::<u128>::new(1);
+    for i in m+1..=*n {
+        *opt_n *= i as u128;
     }
-    Ok((opt_n / factorial(n - m)) as f64)
+    let dif = n - m;
+    Ok(*opt_n as f64 / factorial(&dif) as f64 )
 }
 
 
 #[allow(dead_code)]
-pub fn bernoulli(m: usize, n: usize, p: f64, q: f64) -> Result<f64, Box<dyn Error>> {
+pub fn bernoulli(m: &usize, n: &usize, p: &f64, q: &f64) -> Result<f64, Box<dyn Error>> {
     //! P_n^m = C_n^m * p^m * q^(n-m)
 
     match binominal_coefficient(m, n) {
-        Ok(c) => Ok(c * p.powf(m as f64) * q.powf((n - m) as f64)),
+        Ok(c) => Ok(c * p.powf(*m as f64) * q.powf((n - m) as f64)),
         Err(e) => Err(e)
     }
 }
 
 #[allow(dead_code)]
-pub fn quadratic_equation(a: f64, b: f64, c: f64) -> Result<Option<[f64; 2]>, Box<dyn Error>> {
+pub fn quadratic_equation(a: &f64, b: &f64, c: &f64) -> Result<Option<[f64; 2]>, Box<dyn Error>> {
     //! +-ax^2 +-bx +-с = 0
 
-    if a == 0.0 { return Err("Argument 'a' must not be 0.".into()); }
+    if *a == 0.0 { return Err("Argument 'a' must not be 0.".into()); }
 
     let d = b.powf(2.0) - 4.0 * a * c;
     if d < 0.0 { return  Ok(None) }
@@ -59,11 +60,11 @@ pub fn quadratic_equation(a: f64, b: f64, c: f64) -> Result<Option<[f64; 2]>, Bo
 
 
 #[allow(dead_code)]
-pub fn sigma(n: usize) -> usize {
+pub fn sigma(n: &usize) -> usize {
     //! sigma(n) = 1 + 2 + 3 + ... + n
 
     let mut sum: usize = 0;
-    for i in 1..=n {
+    for i in 1..=*n {
         sum += i;
     }
     sum
@@ -76,25 +77,25 @@ pub struct AProgression {}
 impl AProgression {
 
     #[allow(dead_code)]
-    pub fn sum(a1: f64, d: f64, n: usize) -> f64 {
+    pub fn sum(a1: &f64, d: &f64, n: &usize) -> f64 {
         //! sum = a1 * n + d * sigma(n - 1)
 
-        a1 * n as f64 + d * sigma(n - 1) as f64
+        a1 * *n as f64 + d * sigma(&(n - 1)) as f64
     }
 
     #[allow(dead_code)]
-    pub fn get_n(a1: f64, d: f64, sum: f64) -> usize {
+    pub fn get_n(a1: &f64, d: &f64, sum: &f64) -> usize {
         //! 'n' - number of iterations will be spent before 'sum' is achieved
 
-        let mut n: usize = 1;
-        while AProgression::sum(a1, d, n) < sum {
-            n += 1
+        let mut n = Box::<usize>::new(1);
+        while AProgression::sum(a1, d, &n) < *sum {
+            *n += 1
         }
-        n
+        *n
     }
 
     #[allow(dead_code)]
-    pub fn get_a_n(a_k: f64, d: f64, k: usize, n: usize) -> f64 {
+    pub fn get_a_n(a_k: &f64, d: &f64, k: &usize, n: &usize) -> f64 {
         //! a_n = a_k + (n - k) * d , n => k
         //!
         //! a_n = a_k - (k - n) * d , n <= k
@@ -110,33 +111,49 @@ pub struct GProgression {}
 impl crate::math::GProgression {
 
     #[allow(dead_code)]
-    pub fn sum(b1: f64, q: f64, n: usize) -> Result<f64, Box<dyn Error>> {
+    pub fn sum(b1: &f64, q: &f64, n: &usize) -> Result<f64, Box<dyn Error>> {
         //! sum = b1 * (q^n - 1) / (q - 1)
 
-        if q == 1.0 { return Err("Argument 'q' must not be 1.0".into()) }
-        Ok(b1 * (q.powf(n as f64) - 1.0) / (q - 1.0))
+        if *q == 1.0 { return Err("Argument 'q' must not be 1.0".into()) }
+        Ok(b1 * (q.powf(*n as f64) - 1.0) / (q - 1.0))
     }
 
     #[allow(dead_code)]
-    pub fn get_n(b1: f64, q: f64, sum: f64) -> usize {
+    pub fn get_n(b1: &f64, q: &f64, sum: &f64) -> usize {
         //! 'n' - number of iterations will be spent before 'sum' is achieved
 
-        let mut n: usize = 1;
-        while crate::math::GProgression::sum(b1, q, n).ok() < Some(sum) {
-            n += 1
+        let mut n = Box::<usize>::new(1);
+        while crate::math::GProgression::sum(b1, q, &n).ok() < Some(*sum) {
+            *n += 1
         }
-        n
+        *n
     }
 
     #[allow(dead_code)]
-    pub fn get_b_n(b_k: f64, q: f64, k: usize, n: usize) -> Result<f64, Box<dyn Error>> {
-        //! b_n = b_k * q^(n - k) , n => k
+    pub fn get_b_n(b_k: &f64, q: &f64, k: &usize, n: &usize) -> Result<f64, Box<dyn Error>> {
+        //! b_n = b_k * q^(n - k) , n > k
         //!
-        //! b_n = b_k / q^(k - n) , n <= k
+        //! b_n = b_k / q^(k - n) , n < k
 
-        if q == 0.0 { return Err("Argument 'q' must not be 0".into()) }
+        if *q == 0.0 { return Err("Argument 'q' must not be 0".into()) }
 
         if n > k { return Ok(b_k * q.powf((n - k) as f64)) }
         Ok(b_k / q.powf((k - n) as f64))
+    }
+
+    #[allow(dead_code)]
+    pub fn get_q(b_k: &f64, b_n: &f64, k: &usize, n: &usize) -> Result<f64, Box<dyn Error>> {
+        //! q = (b_n / b_k)^(1 / (n - k)) , n > k
+        //!
+        //! q = (b_k / b_n)^(1 / (k - n)) , n < k
+
+        if n == k { return Err("Argument 'n' must not be equal to 'k'.".into()) }
+
+        if n > k {
+            if *b_k == 0.0 { return Err("Argument 'b_k' must not be 0".into()) }
+            return Ok((b_n / b_k).powf(1.0 / (n - k) as f64))
+        }
+        if *b_n == 0.0 { return Err("Argument 'b_n' must not be 0".into()) }
+        Ok((b_k / b_n).powf(1.0 / (k - n) as f64))
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -5,21 +5,21 @@ mod all_tests {
     #[test]
     fn test_factorial1_pos() {
         // n! = 2 * 3 * ... * n
-        let res = factorial(5);
+        let res = factorial(&5);
         let res_test: u128 = 2 * 3 * 4 * 5;
         assert_eq!(res, res_test);
     }
 
     #[test]
     fn test_factorial2_pos() {
-        let res = factorial(0);
+        let res = factorial(&0);
         let res_test: u128 = 0;
         assert_eq!(res, res_test);
     }
 
     #[test]
     fn test_factorial3_pos() {
-        let res = factorial(1);
+        let res = factorial(&1);
         let res_test: u128 = 1;
         assert_eq!(res, res_test);
     }
@@ -27,14 +27,14 @@ mod all_tests {
     #[test]
     fn test_binominal_coefficient1_pos() {
         // C_n^m = n! / m! * (n - m)!
-        let res = binominal_coefficient(4, 5).unwrap();
+        let res = binominal_coefficient(&4, &5).unwrap();
         let res_test: f64 = (2.0 * 3.0 * 4.0 * 5.0) / (2.0 * 3.0 * 4.0) * (5.0 - 4.0);
         assert_eq!(res, res_test);
     }
 
     #[test]
     fn test_binominal_coefficient1_neg() {
-        let res = binominal_coefficient(6, 5);
+        let res = binominal_coefficient(&6, &5);
         assert!(res.is_err());
     }
 
@@ -43,14 +43,14 @@ mod all_tests {
         // P_n^m = C_n^m * p^m * q^(n-m)
         let q: f64 = 0.5;
         let p: f64 = 0.5;
-        let res = bernoulli(7, 15, p, q).unwrap();
-        let res_test: f64 = binominal_coefficient(7, 15).unwrap() * p.powf(7.0) * q.powf(15.0-7.0);
+        let res = bernoulli(&7, &15, &p, &q).unwrap();
+        let res_test: f64 = binominal_coefficient(&7, &15).unwrap() * p.powf(7.0) * q.powf(15.0-7.0);
         assert_eq!(res, res_test);
     }
 
     #[test]
     fn test_bernoulli1_neg() {
-        let res = bernoulli(15, 7, 0.5, 0.5);
+        let res = bernoulli(&15, &7, &0.5, &0.5);
         assert!(res.is_err());
     }
 
@@ -64,74 +64,73 @@ mod all_tests {
         let x1 = (-b + d.sqrt()) / 2.0 * a;
         let x2 = (-b - d.sqrt()) / 2.0 * a;
         let res_test = Some([x1, x2]);
-
-        let res = quadratic_equation(a, b, c).unwrap();
+        let res = quadratic_equation(&a, &b, &c).unwrap();
         assert_eq!(res, res_test);
     }
 
     #[test]
     fn test_quadratic_equation2_pos() {
-        let res = quadratic_equation(-7.0, 6.0, -2.0).unwrap();
+        let res = quadratic_equation(&-7.0, &6.0, &-2.0).unwrap();
         assert_eq!(res, None);
     }
 
     #[test]
     fn test_quadratic_equation3_pos() {
-        let res = quadratic_equation(-7.0, 0.0, -2.0).unwrap();
+        let res = quadratic_equation(&-7.0, &0.0, &-2.0).unwrap();
         assert_eq!(res, None);
     }
 
     #[test]
     fn test_quadratic_equation4_pos() {
-        let res = quadratic_equation(-7.0, 0.0, 0.0).unwrap();
+        let res = quadratic_equation(&-7.0, &0.0, &0.0).unwrap();
         assert_eq!(res, Some([0.0; 2]));
     }
 
     #[test]
     fn test_quadratic_equation1_neg() {
-        let res = quadratic_equation(0.0, 6.0, -2.0);
+        let res = quadratic_equation(&0.0, &6.0, &-2.0);
         assert!(res.is_err());
     }
 
     #[test]
     fn test_sigma1_pos() {
         // sigma(n) = 1 + 2 + 3 + ... + n
-        let res = sigma(5);
+        let res = sigma(&5);
         let res_test = 1 + 2 + 3 + 4 + 5;
         assert_eq!(res, res_test);
     }
 
     #[test]
     fn test_sigma1_neg() {
-        let res = sigma(5);
+        let res = sigma(&5);
         assert_ne!(res, 2);
     }
 
         #[test]
     fn test_aprogression_sum1_pos() {
         // sum = a1 * n + d * sigma(n - 1)
-        let res = AProgression::sum(2.0, 1.5, 4);
+        let res = AProgression::sum(&2.0, &1.5, &4);
         let res_test = 2.0 * 4.0 + 1.5 * (3.0 + 2.0 + 1.0);
         assert_eq!(res, res_test);
     }
 
     #[test]
     fn test_aprogression_sum1_neg() {
-        let res = AProgression::sum(2.0, 1.2, 4);
+        let res = AProgression::sum(&2.0, &1.2, &4);
         let res_test = 2.0 * 4.0 + 1.5 * (3.0 + 2.0 + 1.0);
         assert_ne!(res, res_test);
     }
 
     #[test]
     fn test_aprogression_get_n1_pos() {
-        let res = AProgression::get_n(60.0, 17.0, 1000.0);
+        let res = AProgression::get_n(&60.0, &17.0, &1000.0);
         assert_eq!(res, 9);
     }
 
     #[test]
     fn test_aprogression_get_a_n1_pos() {
         // a_n = a_k + (n - k) * d , n => k
-        let res = AProgression::get_a_n(3.0, 2.0, 2, 6);
+        let res = AProgression::get_a_n(&3.0, &2.0, &2, &6);
         let res_test = 3.0 + (6 - 2) as f64 * 2.0;
         assert_eq!(res, res_test);
     }
@@ -139,7 +138,7 @@ mod all_tests {
     #[test]
     fn test_aprogression_get_a_n2_pos() {
         // a_n = a_k - (k - n) * d , n <= k
-        let res = AProgression::get_a_n(3.0, 2.0, 6, 2);
+        let res = AProgression::get_a_n(&3.0, &2.0, &6, &2);
         let res_test = 3.0 - (6 - 2) as f64 * 2.0;
         assert_eq!(res, res_test);
     }
@@ -148,7 +147,7 @@ mod all_tests {
     #[test]
     fn test_gprogression_sum1_pos() {
         // sum = b1 * (q^n - 1) / (q - 1)
-        let res = GProgression::sum(3.0, 1.15, 5).unwrap();
+        let res = GProgression::sum(&3.0, &1.15, &5).unwrap();
         let q: f64 = 1.15;
         let res_test = 3.0 * (q.powf(5.0) - 1.0) / (q - 1.0);
         assert_eq!(res, res_test);
@@ -156,7 +155,7 @@ mod all_tests {
 
     #[test]
     fn test_gprogression_sum1_neg() {
-        let res = GProgression::sum(3.0, 1.15, 5).unwrap();
+        let res = GProgression::sum(&3.0, &1.15, &5).unwrap();
         let q: f64 = 1.15;
         let res_test = 3.0 * (q.powf(5.0) - 2.0) / (q - 1.0);
         assert_ne!(res, res_test);
@@ -164,20 +163,20 @@ mod all_tests {
 
     #[test]
     fn test_gprogression_sum2_neg() {
-        let res = GProgression::sum(3.0, 1.0, 5);
+        let res = GProgression::sum(&3.0, &1.0, &5);
         assert!(res.is_err());
     }
 
     #[test]
     fn test_gprogression_get_n1_pos() {
-        let res = GProgression::get_n(3.0, 1.15, 20.0);
+        let res = GProgression::get_n(&3.0, &1.15, &20.0);
         assert_eq!(res, 5);
     }
 
     #[test]
     fn test_gprogression_get_b_n1_pos() {
-        // b_n = b_k * q^(n - k) , n => k
-        let res = GProgression::get_b_n(3.0, 1.15, 1, 6).unwrap();
+        // b_n = b_k * q^(n - k) , n > k
+        let res = GProgression::get_b_n(&3.0, &1.15, &1, &6).unwrap();
         let q: f64 = 1.15;
         let res_test = 3.0 * (q.powf(6.0 - 1.0));
         assert_eq!(res, res_test);
@@ -185,8 +184,8 @@ mod all_tests {
 
     #[test]
     fn test_gprogression_get_b_n2_pos() {
-        // b_n = b_k / q^(k - n) , n <= k
-        let res = GProgression::get_b_n(3.0, 1.15, 6, 1).unwrap();
+        // b_n = b_k / q^(k - n) , n < k
+        let res = GProgression::get_b_n(&3.0, &1.15, &6, &1).unwrap();
         let q: f64 = 1.15;
         let res_test = 3.0 / (q.powf(6.0 - 1.0));
         assert_eq!(res, res_test);
@@ -194,7 +193,43 @@ mod all_tests {
 
     #[test]
     fn test_gprogression_get_b_n2_neg() {
-        let res = GProgression::get_b_n(3.0, 0.0, 6, 1);
+        let res = GProgression::get_b_n(&3.0, &0.0, &6, &1);
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_gprogression_get_q1_pos() {
+        // q = (b_n / b_k)^(1 / (n - k)) , n > k
+        let res = GProgression::get_q(&32.0, &1024.0, &2, &3).unwrap();
+        let div: f64 = 1024.0 / 32.0;
+        let res_test = div.powf(1.0 / (3.0 - 2.0));
+        assert_eq!(res, res_test);
+    }
+
+    #[test]
+    fn test_gprogression_get_q2_pos() {
+        // q = (b_k / b_n)^(1 / (k - n)) , n < k
+        let res = GProgression::get_q(&1024.0, &32.0, &3, &2).unwrap();
+        let div: f64 = 1024.0 / 32.0;
+        let res_test = div.powf(1.0 / (3.0 - 2.0));
+        assert_eq!(res, res_test);
+    }
+
+    #[test]
+    fn test_gprogression_get_q1_neg() {
+        let res = GProgression::get_q(&32.0, &1024.0, &3, &3);
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_gprogression_get_q2_neg() {
+        let res = GProgression::get_q(&32.0, &0.0, &3, &1);
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_gprogression_get_q3_neg() {
+        let res = GProgression::get_q(&0.0, &1.0, &1, &3);
         assert!(res.is_err());
     }
 }
